c5c13a55be169ae452fc3f87da4d99b9349a5342
Cristhian Leandro Góngora Torrez <crisgongor7@gmail.com>
2024-09-23 10:35:11 -0400

    Formateado de código para el modulo common

--- src/common/dtos/pagination.dto.ts
+++ src/common/dtos/pagination.dto.ts
@@ -1,16 +1,14 @@
-import { Type } from "class-transformer";
-import { IsOptional, IsPositive } from "class-validator";
+import { Type } from 'class-transformer';
+import { IsOptional, IsPositive } from 'class-validator';
 
+export class PaginationDto {
+  @IsPositive()
+  @IsOptional()
+  @Type(() => Number)
+  page?: number;
 
-export class PaginationDto{
-
-    @IsPositive()
-    @IsOptional()
-    @Type(() => Number)
-    page?: number;
-
-    @IsPositive()
-    @IsOptional()
-    @Type(() => Number)
-    limit?: number;
+  @IsPositive()
+  @IsOptional()
+  @Type(() => Number)
+  limit?: number;
 }
