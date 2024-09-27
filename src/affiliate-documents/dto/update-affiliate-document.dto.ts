cc9144842012f58b5898157b04cec76ef716bc44
ferbautistamollo <ferbautistamollo@gmail.com>
2024-09-27 09:16:06 -0400

    Eliminación de archivos adicionales

--- src/affiliate-documents/dto/update-affiliate-document.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAffiliateDocumentDto } from './create-affiliate-document.dto';
-
-export class UpdateAffiliateDocumentDto extends PartialType(CreateAffiliateDocumentDto) {
-  id: number;
-}
