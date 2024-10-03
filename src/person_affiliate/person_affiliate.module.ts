e6d23e9cf02530f0bec7cec5b2f4b215eb577fee
Cristhian Leandro Góngora Torrez <crisgongor7@gmail.com>
2024-10-03 11:31:51 -0400

    importación de entidad PersonAffiliate en el modulo

--- src/person_affiliate/person_affiliate.module.ts
+++ src/person_affiliate/person_affiliate.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PersonAffiliateService } from './person_affiliate.service';
 import { PersonAffiliateController } from './person_affiliate.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PersonAffiliate } from './entities/person_affiliate.entity';
 
 @Module({
   controllers: [PersonAffiliateController],
   providers: [PersonAffiliateService],
+  imports: [TypeOrmModule.forFeature([PersonAffiliate])],
 })
 export class PersonAffiliateModule {}
