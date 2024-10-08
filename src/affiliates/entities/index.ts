4ff0035e564b3599ff8dc52ce6cb9cb8eca8acc2
ferbautistamollo <ferbautistamollo@gmail.com>
2024-10-08 16:14:32 -0400

    Barrel pattern - Re-expotación del modulo afiliado

---
+++ src/affiliates/entities/index.ts
@@ -0,0 +1,4 @@
+export { Affiliate } from './affiliate.entity';
+export { AffiliateState } from './affiliate-state.entity';
+export { AffiliateDocument } from './affiliate-document.entity';
+export { stateType } from './state-type.entity';
