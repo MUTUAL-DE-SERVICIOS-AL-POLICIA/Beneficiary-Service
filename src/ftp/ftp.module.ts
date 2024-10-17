78f29f327f5b6374d4e31447e7e375b239831e90
ferbautistamollo <ferbautistamollo@gmail.com>
2024-10-17 10:11:45 -0400

    Creacion de Servicio GLobal para la comunicaicon con el servidor FTP y configuracion de MOdulo

---
+++ src/ftp/ftp.module.ts
@@ -0,0 +1,9 @@
+import { Global, Module } from '@nestjs/common';
+import { FtpService } from './ftp.service';
+
+@Global()
+@Module({
+  providers: [FtpService],
+  exports: [FtpService]
+})
+export class FtpModule {}
