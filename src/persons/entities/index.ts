845e962a7e657152900d27e3c4e0f6c521240eef
gongor7 <crisgongor7@gmail.com>
2024-10-18 14:49:07 -0400

    Archivo de Barril para exportar entidades de persona

--- src/persons/entities/index.ts
+++ src/persons/entities/index.ts
@@ -1,2 +1,4 @@
 export { Person } from './person.entity';
 export { PersonAffiliate } from './person-affiliate.entity';
+export { PersonFingerprint } from './person-fingerprint.entity';
+export { FingerprintType } from './fingerprint-type.entity';
