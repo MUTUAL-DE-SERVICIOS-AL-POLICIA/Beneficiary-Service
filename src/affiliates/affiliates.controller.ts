bd9d0d739707c98e769370b6017bb66e9006ab87
ferbautistamollo <ferbautistamollo@gmail.com>
2024-10-18 17:35:01 -0400

    End Point Para mostrar datos del afiliado

--- src/affiliates/affiliates.controller.ts
+++ src/affiliates/affiliates.controller.ts
@@ -1,28 +1,11 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  Query,
-  ParseIntPipe,
-  UsePipes,
-} from '@nestjs/common';
+import { Controller, Get, Query, ParseIntPipe, UsePipes } from '@nestjs/common';
 import { AffiliatesService } from './affiliates.service';
-import { CreateAffiliateDto, UpdateAffiliateDto } from './dto';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { FileRequiredPipe } from './pipes/file-required.pipe';
 @Controller('affiliates')
 export class AffiliatesController {
   constructor(private readonly affiliatesService: AffiliatesService) {}
-
-  @Post()
-  create(@Body() createAffiliateDto: CreateAffiliateDto) {
-    return this.affiliatesService.create(createAffiliateDto);
-  }
 
   @Get()
   findAll(@Query() paginationDto: PaginationDto) {
@@ -31,17 +14,12 @@ export class AffiliatesController {
   }
   @MessagePattern('affiliate.findOne')
   findOne(@Payload('id', ParseIntPipe) id: number) {
-    return this.affiliatesService.findOne(+id);
+    return this.affiliatesService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAffiliateDto: UpdateAffiliateDto) {
-    return this.affiliatesService.update(+id, updateAffiliateDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.affiliatesService.remove(+id);
+  @MessagePattern('affiliate.findOneData')
+  findOneData(@Payload('id', ParseIntPipe) id: number) {
+    return this.affiliatesService.findOneData(id);
   }
 
   @MessagePattern('affiliate.createDocuments')
