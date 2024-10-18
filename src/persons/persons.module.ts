0fb50474c7803a50da9d01347091d2d03a9a3158
gongor7 <crisgongor7@gmail.com>
2024-10-18 14:50:13 -0400

    Importación de las entidades para huellas con typeORM

--- src/persons/persons.module.ts
+++ src/persons/persons.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { PersonsService } from './persons.service';
 import { PersonsController } from './persons.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Person, PersonAffiliate } from './entities';
+import { FingerprintType, Person, PersonAffiliate, PersonFingerprint } from './entities';
 import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
   controllers: [PersonsController],
   providers: [PersonsService],
-  imports: [TypeOrmModule.forFeature([Person, PersonAffiliate]), NatsModule],
+  imports: [
+    TypeOrmModule.forFeature([Person, PersonAffiliate, FingerprintType, PersonFingerprint]),
+    NatsModule,
+  ],
 })
 export class PersonsModule {}
