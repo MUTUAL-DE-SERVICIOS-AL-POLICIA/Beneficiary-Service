4c2750ca43ae00097892cb606da0b8c70124a4d3
ferbautistamollo <ferbautistamollo@gmail.com>
2024-10-21 16:47:17 -0400

    Ajuste de importacion no requerido de ftp

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,10 @@ import { PersonsModule } from './persons/persons.module';
 import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
 import { AffiliatesModule } from './affiliates/affiliates.module';
-import { FtpModule } from './ftp/ftp.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    DatabaseModule,
-    PersonsModule,
-    CommonModule,
-    AffiliatesModule,
-    FtpModule,
-  ],
+  imports: [ConfigModule.forRoot(), DatabaseModule, PersonsModule, CommonModule, AffiliatesModule],
   controllers: [],
   providers: [],
 })
