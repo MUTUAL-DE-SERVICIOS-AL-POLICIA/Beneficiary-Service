e6d6e8a167e6d2f9bc9b332dfb1c0e929ecb36d5
Adrian-Herrera-96 <adrirha@gmail.com>
2024-10-21 14:22:47 -0400

    Configuración para permitir seeders

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PersonsModule } from './persons/persons.module';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommonModule } from './common/common.module';
 import { AffiliatesModule } from './affiliates/affiliates.module';
 import { FtpModule } from './ftp/ftp.module';
-import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      database: process.env.DB_NAME,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      autoLoadEntities: true,
-      synchronize: true,
-      namingStrategy: new SnakeNamingStrategy(),
-    }),
+    DatabaseModule,
     PersonsModule,
     CommonModule,
     AffiliatesModule,
