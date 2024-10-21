1c0e6416d5d1ebbbe9f5c6ee3da555d63d36644f
ferbautistamollo <ferbautistamollo@gmail.com>
2024-10-21 16:53:10 -0400

    creacion de archivo de barril para rutas comunes

---
+++ src/common/index.ts
@@ -0,0 +1,6 @@
+export { PaginationDto } from './dtos/pagination.dto';
+
+export { FtpService } from './ftp/ftp.service';
+export { NatsService } from './nats/nats.service';
+
+export { FileRequiredPipe } from './pipes/file-required.pipe';
