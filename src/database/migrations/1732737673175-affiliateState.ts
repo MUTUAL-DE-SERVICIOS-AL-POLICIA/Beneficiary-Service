7d3908d92a94d78284b884f740b72b34aa036529
ferbautistamollo <ferbautistamollo@gmail.com>
2024-11-28 16:37:33 -0400

    migracion tabala affiliate state

---
+++ src/database/migrations/1732737673175-affiliateState.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class affiliateState1732737673175 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        schema: 'beneficiaries',
+        name: 'affiliate_states',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            isNullable: false,
+          },
+          {
+            name: 'name',
+            type: 'text',
+            isNullable: false,
+          },
+          {
+            name: 'state_type_id',
+            type: 'int',
+            isNullable: false,
+          },
+        ],
+      }),
+      true,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('beneficiaries.state_types', true);
+  }
+}
