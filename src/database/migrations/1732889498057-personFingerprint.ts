958c8cb4af8cad7c27be665ddac591610a04e8fc
ferbautistamollo <ferbautistamollo@gmail.com>
2024-11-29 13:46:20 -0400

    migracion creacion tabla personhuellas

---
+++ src/database/migrations/1732889498057-personFingerprint.ts
@@ -0,0 +1,59 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class personFingerprint1732889498057 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        schema: 'beneficiaries',
+        name: 'person_fingerprints',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'quality',
+            type: 'int',
+          },
+          {
+            name: 'path',
+            type: 'text',
+          },
+          {
+            name: 'person_id',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'fingerprint_type_id',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+            onUpdate: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'deleted_at',
+            type: 'timestamp',
+            isNullable: true,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('beneficiaries.person_fingerprints');
+  }
+}
