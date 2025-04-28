149de064a1d439f965d4507adc0f42f5c67bdc20
Adrian-Herrera-96 <adrirha@gmail.com>
2025-04-28 09:51:56 -0400

    Actualización addUuid migration

--- src/database/migrations/1738340797410-addUuidAffiliatesAndSpouse.ts
+++ src/database/migrations/1738340797410-addUuidAffiliatesAndSpouse.ts
@@ -3,24 +3,30 @@ import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 export class BeneficiaryAddUuidAffiliatesAndSpouse1738340797410 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`);
-    await queryRunner.addColumn(
-      'public.affiliates',
-      new TableColumn({
-        name: 'uuid_reference', // Nombre de la columna
-        type: 'UUID', // Tipo de dato
-        isNullable: false,
-        default: 'uuid_generate_v4()', // Valor por defecto
-      }),
-    );
-    await queryRunner.addColumn(
-      'public.spouses',
-      new TableColumn({
-        name: 'uuid_column', // Nombre de la columna
-        type: 'UUID', // Tipo de dato
-        isNullable: false,
-        default: 'uuid_generate_v4()', // Valor por defecto
-      }),
-    );
+    const hasColumn = await queryRunner.hasColumn('public.affiliates', 'uuid_reference');
+    if(!hasColumn){
+      await queryRunner.addColumn(
+        'public.affiliates',
+        new TableColumn({
+          name: 'uuid_reference', // Nombre de la columna
+          type: 'UUID', // Tipo de dato
+          isNullable: false,
+          default: 'uuid_generate_v4()', // Valor por defecto
+        }),
+      );
+    }
+    const hasColumn2 = await queryRunner.hasColumn('public.spouses', 'uuid_column');
+    if(!hasColumn2){
+      await queryRunner.addColumn(
+        'public.spouses',
+        new TableColumn({
+          name: 'uuid_column', // Nombre de la columna
+          type: 'UUID', // Tipo de dato
+          isNullable: false,
+          default: 'uuid_generate_v4()', // Valor por defecto
+        }),
+      );
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
